cf3ed84b7871a3bd3bf4e35ef046a6dc3c798ef8
pedrobarretto <pedro@barretto.com.br>
2023-04-17 08:11:32 -0300

    chore: add prettier and create schema file

--- src/server.ts
+++ src/server.ts
@@ -1,20 +1,28 @@
-import express from 'express';
 import { ApolloServer } from 'apollo-server-express';
+import express from 'express';
 import { makeExecutableSchema } from 'graphql-tools';
-import { resolvers } from './resolvers';
+
 import { connect } from './mongo';
+import { resolvers } from './resolvers';
 import { typeDefs } from './schema';
+import { printSchema } from 'graphql';
+import fs from 'fs';
 
 async function startApolloServer() {
-
   connect();
 
   // create the Apollo Server instance
-  const server = new ApolloServer({
-    schema: makeExecutableSchema({
-      typeDefs,
-      resolvers,
-    }),
+  const schema = makeExecutableSchema({
+    typeDefs,
+    resolvers,
+  });
+  const server = new ApolloServer({ schema });
+
+  const schemaString = printSchema(schema);
+
+  fs.writeFile('schema.graphql', schemaString, (err) => {
+    if (err) throw err;
+    console.log('Schema file created!');
   });
 
   const app = express();
@@ -23,8 +31,11 @@ async function startApolloServer() {
 
   server.applyMiddleware({ app });
 
-  await new Promise<void>(resolve => app.listen({ port: 4000 }, resolve));
-  console.log(`🚀 Server ready at http://localhost:4000${server.graphqlPath}`);
+  app.listen(4000, () => {
+    console.log(
+      `🚀 Server ready at http://localhost:4000${server.graphqlPath}`,
+    );
+  });
 }
 
 startApolloServer();
